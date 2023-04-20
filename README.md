ab5e399141d5cdd95bda175806e6e51546f578c8
Andrew Shulaev <ash.drone@gmail.com>
2023-04-20 13:53:47 +0100

    Show path length; if path is blocked, show a red message

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A simple optimization game
 ## Tasks
 
 * [x] Refactor Point to be a class, not a plain object
-* [ ] Implement shortest path search and display the current path length in the UI
+* [x] Implement shortest path search and display the current path length in the UI
 * [x] Highlight the cells that are a part of the shortest path
 * [ ] Draw the UI in a prettier way, maybe using some sprites
 * [ ] Implement support for partial grids
--- src/grid.ts
+++ src/grid.ts
@@ -201,10 +201,18 @@ export class Grid implements m.ClassComponent<GridAttrs> {
     );
   }
 
+  renderPath(path: GridPath|undefined): m.Child {
+    return m('span' + (path === undefined ? '.red' : ''),
+      this.gridLogic.path === undefined ? 'Path blocked!' : `Path length: ${this.gridLogic.path.points.length}`);
+  }
+
   view({attrs}: m.Vnode<GridAttrs>): m.Child {
-    return m('table',
-      this.gridLogic.cells.map((rowContent, row) =>
-        m('tr', rowContent.map((value, col) => this.renderCell(attrs, new Point(row, col), value))))
+    return m('div',
+      m('table',
+        this.gridLogic.cells.map((rowContent, row) =>
+          m('tr', rowContent.map((value, col) => this.renderCell(attrs, new Point(row, col), value))))
+      ),
+      this.renderPath(this.gridLogic.path)
     );
   }
 }
