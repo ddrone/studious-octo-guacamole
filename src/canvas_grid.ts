e9dd80cd466defc051af1a49ae1fffba4a757ad7
Andrew Shulaev <ash.drone@gmail.com>
2023-05-01 09:54:08 +0100

    Start rendering things on a canvas

--- src/canvas_grid.ts
+++ src/canvas_grid.ts
@@ -20,15 +20,38 @@ export class CanvasGrid implements m.ClassComponent<GridAttrs> {
     this.gridLogic = new GridLogic(vnode.attrs);
   }
 
+  renderGrid() {
+    const ctx = this.ctx;
+    ctx.fillStyle = '#eee';
+    for (let i = 0; i <= this.gridLogic.rows; i++) {
+      ctx.fillRect(0, i * (cellSize + gapSize), this.canvasWidth, gapSize);
+    }
+    for (let i = 0; i <= this.gridLogic.columns; i++) {
+      ctx.fillRect(i * (cellSize + gapSize), 0, gapSize, this.canvasHeight);
+    }
+  }
+
+  onupdate() {
+    const ctx = this.ctx;
+    ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.renderGrid();
+  }
+
   view(vnode: m.Vnode<GridAttrs>): m.Child {
-    return m('canvas', {
-      oncreate: (vnode: m.VnodeDOM) => {
-        const canvas = vnode.dom as HTMLCanvasElement;
-        this.ctx = canvas.getContext("2d")!;
-      },
-      width: this.canvasWidth,
-      height: this.canvasHeight,
-      border: 1,
-    });
+    return m('div',
+      m('canvas', {
+        oncreate: (vnode: m.VnodeDOM) => {
+          const canvas = vnode.dom as HTMLCanvasElement;
+          this.ctx = canvas.getContext("2d")!;
+        },
+        width: this.canvasWidth,
+        height: this.canvasHeight,
+        border: 1,
+      }),
+      m('button', {
+        onclick: () => {
+          console.log('What a hack')
+        }
+      }, 'Update'));
   }
 }
