7cf4be43d47376da021f706d0d169458a4e8b922
Andrew Shulaev <ash.drone@gmail.com>
2023-05-01 10:17:51 +0100

    Draw the goal as well

--- src/canvas_grid.ts
+++ src/canvas_grid.ts
@@ -1,8 +1,17 @@
 import m from 'mithril';
 import { GridAttrs, GridLogic } from './grid_logic';
+import { starPath } from './canvas_utils';
 
 const cellSize = 50;
 const gapSize = 5;
+
+function cellOffset(coord: number): number {
+  return (cellSize + gapSize) * coord + gapSize;
+}
+
+function cellCenter(coord: number): number {
+  return cellOffset(coord) + cellSize / 2;
+}
 
 export class CanvasGrid implements m.ClassComponent<GridAttrs> {
   ctx: CanvasRenderingContext2D = undefined as any;
@@ -35,12 +44,25 @@ export class CanvasGrid implements m.ClassComponent<GridAttrs> {
     const ctx = this.ctx;
     ctx.fillStyle = '#666';
     const start = this.gridLogic.start;
-    const x = start.row * (cellSize + gapSize) + gapSize + cellSize / 2;
-    const y = start.col * (cellSize + gapSize) + gapSize + cellSize / 2;
+    const x = cellCenter(start.col);
+    const y = cellCenter(start.row);
     const r = cellSize * 0.45;
 
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  renderEnd() {
+    const ctx = this.ctx;
+    ctx.fillStyle = '#f66';
+    const end = this.gridLogic.end;
+    const x = cellCenter(end.col);
+    const y = cellCenter(end.row);
+    const outerR = cellSize * 0.45;
+    const innerR = cellSize * 0.15;
+
+    starPath(ctx, x, y, outerR, innerR, 5);
     ctx.fill();
   }
 
@@ -49,6 +71,7 @@ export class CanvasGrid implements m.ClassComponent<GridAttrs> {
     ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.renderGrid();
     this.renderStart();
+    this.renderEnd();
   }
 
   view(vnode: m.Vnode<GridAttrs>): m.Child {
